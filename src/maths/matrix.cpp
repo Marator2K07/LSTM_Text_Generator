15799246f430e515f8aecf1251ef76913a2c5787
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-18 16:16:43 +0300

    Добавлено поле для размеров матрицы + геттеры для него.

--- src/maths/matrix.cpp
+++ src/maths/matrix.cpp
@@ -2,6 +2,21 @@
 
 Matrix::Matrix(QObject *parent)
     : QObject{parent}
+    , sizes{0,0,0}
 {
+}
 
+const int Matrix::width()
+{
+    return sizes[0];
+}
+
+const int Matrix::height()
+{
+    return sizes[1];
+}
+
+const int Matrix::depth()
+{
+    return sizes[2];
 }
--- src/maths/matrix.h
+++ src/maths/matrix.h
@@ -2,12 +2,19 @@
 #define MATRIX_H
 
 #include <QObject>
+#include <constants.h>
 
 class Matrix : public QObject
 {
     Q_OBJECT
+private:
+    int sizes[MAX_MATRIX_SIZE];
+
 public:
     explicit Matrix(QObject *parent = nullptr);
+    const int width();
+    const int height();
+    const int depth();
 
 signals:
 
