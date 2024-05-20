93f905a215cc6acd761066526941cda8d0ae0c79
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-20 20:34:24 +0300

    Добавлен метод гарантированного возврата числа с градиентом.

--- src/lstm/numwithgradient.cpp
+++ src/lstm/numwithgradient.cpp
@@ -7,6 +7,18 @@ NumWithGradient::NumWithGradient(double num)
 {
 }
 
+NumWithGradient NumWithGradient::ensureNum(Num *num)
+{
+    switch (num->type()) {
+    case Num_Type::Simple:
+        return NumWithGradient(num->num());
+    case Num_Type::WithGradient:
+        return *(NumWithGradient*)num;
+    default:
+        return NumWithGradient(0);
+    }
+}
+
 double NumWithGradient::num() const
 {
     return num_;
--- src/lstm/numwithgradient.h
+++ src/lstm/numwithgradient.h
@@ -16,11 +16,19 @@ private:
     vector<double> depends_on;
     QString creation_op;
 
+private:
+    ///
+    /// \brief ensureNum вспомогательный метод для
+    /// превращения любого числа на входе в число
+    /// с градиентом и его возвращение
+    /// \param num - пришедшее абстрактное число
+    /// с градиентом или без
+    NumWithGradient ensureNum(Num *num);
+
 public:
     explicit NumWithGradient(double num);
 
 public:
-
     // Num interface
     double num() const override;
     Num_Type type() const override;
