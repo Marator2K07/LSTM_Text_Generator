f2b84129053c383626c64d489513fbb12f34dec1
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-24 19:24:30 +0300

    Добавлена вторая проверка для класса NumWithGradient .

--- test/test.cpp
+++ test/test.cpp
@@ -14,6 +14,7 @@ private slots:
     /// простого 'уравнения' и его тестирование с помощью
     /// нахождения градиента (производной)
     void testNumWithGradientOne();
+    void testNumWithGradientTwo();
 };
 
 void Test::testNumWithGradientOne()
@@ -31,6 +32,27 @@ void Test::testNumWithGradientOne()
     // на данный момент у нас уравнение вида:
     // c = (a * 4) + 5, где при а = 3, градиент = 4
     double answer = 4;
+    QCOMPARE(answer, a.gradient());
+}
+
+void Test::testNumWithGradientTwo()
+{
+    NumWithGradient a(3);
+
+    SimpleNum forB(4);
+    NumWithGradient b(a * (Num *)(&forB));
+
+    SimpleNum forC(3);
+    NumWithGradient c(b + (Num *)(&forC));
+
+    SimpleNum forD(2);
+    NumWithGradient d(a + (Num *)(&forD));
+
+    NumWithGradient e(c * (Num *)&d);
+
+    e.backward();
+
+    double answer = 35;
     QCOMPARE(answer, a.gradient());
 }
 
