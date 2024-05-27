7019beb854b5c6c3bf35d6ad00c61b4e18ad8206
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-27 17:51:07 +0300

    Добавлен метод многопеременной логистической функции.
    с реализацией.

--- src/loss/lossactivationfunctions.cpp
+++ src/loss/lossactivationfunctions.cpp
@@ -1,3 +1,16 @@
 #include "lossactivationfunctions.h"
 
-
+vector<double> LossActivationFunctions::softmax(const vector<double> p)
+{
+    // сначала находим общий знаменатель
+    double denominator = 0;
+    for (const double value : p) {
+        denominator += exp(value);
+    }
+    // следующим проходом составляем обработанный вектор
+    vector<double> result;
+    for (const double value : p) {
+        result.push_back(exp(value)/denominator);
+    }
+    return result;
+}
--- src/loss/lossactivationfunctions.h
+++ src/loss/lossactivationfunctions.h
@@ -1,12 +1,21 @@
 #ifndef LOSSACTIVATIONFUNCTIONS_H
 #define LOSSACTIVATIONFUNCTIONS_H
 
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
 ///
 /// \brief The LossActivationFunctions class
 /// для определения функций активации потерь
 class LossActivationFunctions
 {
 public:
+    ///
+    /// \brief softmax многопеременная логистическая функция
+    /// \param p вектор вероятностей
+    static vector<double> softmax(const vector<double> p);
 };
 
 #endif // LOSSACTIVATIONFUNCTIONS_H
