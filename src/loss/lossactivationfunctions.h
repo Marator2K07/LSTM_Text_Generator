b747b53e57dd2a43cf71ca13559d619606cc8014
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-27 15:05:46 +0300

    Создан класс под функции активации потерь.
    Пока ни одной нет.

---
+++ src/loss/lossactivationfunctions.h
@@ -0,0 +1,12 @@
+#ifndef LOSSACTIVATIONFUNCTIONS_H
+#define LOSSACTIVATIONFUNCTIONS_H
+
+///
+/// \brief The LossActivationFunctions class
+/// для определения функций активации потерь
+class LossActivationFunctions
+{
+public:
+};
+
+#endif // LOSSACTIVATIONFUNCTIONS_H
