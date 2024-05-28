0c5365480eb5d815ca09c845a87415713d8aa6c9
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-28 10:34:25 +0300

    Исправление/дополнение в комментариях.

--- src/loss/lossactivationfunctions.h
+++ src/loss/lossactivationfunctions.h
@@ -8,13 +8,13 @@ using namespace std;
 
 ///
 /// \brief The LossActivationFunctions class
-/// для определения функций активации потерь
+/// для определения функций активации
 class LossActivationFunctions
 {
 public:
     ///
     /// \brief softmax многопеременная логистическая функция
-    /// \param p вектор вероятностей
+    /// \param p вектор вероятностей(прогнозов)
     static vector<double> softmax(const vector<double> p);
 };
 
