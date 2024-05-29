33c288884811fca4270b2dc6cf7b8c761288a755
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-29 19:38:28 +0300

    Написана реализация метода forward

--- src/loss/softmaxcrossentropyloss.cpp
+++ src/loss/softmaxcrossentropyloss.cpp
@@ -8,7 +8,16 @@ SoftmaxCrossEntropyLoss::SoftmaxCrossEntropyLoss()
 double SoftmaxCrossEntropyLoss::forward(vector<vector<double>> prediction,
                                         vector<vector<double>> target)
 {
-
+    // проверки
+    if (prediction.size() != target.size() ||
+        prediction[0].size() != target.size()) {
+        return 0.0;
+    }
+    // остальные операции...
+    _prediction = prediction;
+    _target = target;
+    // возвращаем расчитанную потерю
+    return calculate();
 }
 
 vector<vector<double> > SoftmaxCrossEntropyLoss::backward()
