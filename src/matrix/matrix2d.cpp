ed58b34e519846aee9e05cc1a28f99c7f079d0bf
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-31 09:12:31 +0300

    Теперь данный класс работает только с двумерными векторами.
    + расширена функциональность класса.

---
+++ src/matrix/matrix2d.cpp
@@ -0,0 +1,57 @@
+#include <matrix2d.h>
+
+
+template<typename T>
+bool Matrix2d<T>::sameShape(const vector<vector<T>> matrixA,
+                            const vector<vector<T>> matrixB)
+{
+
+}
+
+template<typename T>
+vector<vector<T>> Matrix2d<T>::addition(const vector<vector<T>> matrixA,
+                                        const vector<vector<T> > matrixB)
+{
+
+}
+
+template<typename T>
+vector<vector<T>> Matrix2d<T>::addition(const vector<vector<T>> matrix,
+                                        T num)
+{
+
+}
+
+template<typename T>
+vector<vector<T>> Matrix2d<T>::subtraction(const vector<vector<T>> matrixA,
+                                           const vector<vector<T>> matrixB)
+{
+
+}
+
+template<typename T>
+vector<vector<T>> Matrix2d<T>::subtraction(const vector<vector<T>> matrix,
+                                           T num)
+{
+
+}
+
+template<typename T>
+vector<vector<T>> Matrix2d<T>::multiplication(const vector<vector<T>> matrixA,
+                                              const vector<vector<T>> matrixB)
+{
+
+}
+
+template<typename T>
+vector<vector<T>> Matrix2d<T>::multiplication(const vector<vector<T>> matrix,
+                                              T num)
+{
+
+}
+
+template<typename T>
+vector<vector<T>> Matrix2d<T>::transposition(const vector<vector<T>> matrix)
+{
+
+}
---
+++ src/matrix/matrix2d.h
@@ -0,0 +1,47 @@
+#ifndef MATRIX2D_H
+#define MATRIX2D_H
+
+#include <constants.h>
+#include <vector>
+
+using namespace std;
+
+///
+/// \brief The Matrix2d class
+/// определение основных типов операций
+/// над элементами двумерного матричного типа
+template<typename T>
+class Matrix2d
+{
+public:
+    // соответствие матриц
+    static bool sameShape(const vector<vector<T>> matrixA,
+                          const vector<vector<T>> matrixB);
+
+    // сумма матриц (или матрицы и числа)
+    static vector<vector<T>>
+        addition(const vector<vector<T>> matrixA,
+                 const vector<vector<T>> matrixB);    
+    static vector<vector<T>>
+        addition(const vector<vector<T>> matrix, T num);
+
+    // разность матриц (или матрицы и числа)
+    static vector<vector<T>>
+        subtraction(const vector<vector<T>> matrixA,
+                    const vector<vector<T>> matrixB);    
+    static vector<vector<T>>
+        subtraction(const vector<vector<T>> matrix, T num);
+
+    // умножение матриц (или матрицы и числа)
+    static vector<vector<T>>
+        multiplication(const vector<vector<T>> matrixA,
+                       const vector<vector<T>> matrixB);    
+    static vector<vector<T>>
+        multiplication(const vector<vector<T>> matrix, T num);
+
+    // транспозиция матрицы
+    static vector<vector<T>>
+        transposition(const vector<vector<T>> matrix);
+};
+
+#endif // MATRIX2D_H
