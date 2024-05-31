7d7d39fd2191734d6be018b4a889ecdd762d4415
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-31 09:04:52 +0300

    Выделили класс для трехмерных матриц со стат. методами.
    Методы пока без реализаций.

---
+++ src/matrix/matrix3d.cpp
@@ -0,0 +1,56 @@
+#include "matrix3d.h"
+
+template<typename T>
+bool Matrix3d<T>::sameShape(const vector<vector<vector<T>>> matrixA,
+                            const vector<vector<vector<T>>> matrixB)
+{
+
+}
+
+template<typename T>
+vector<vector<vector<T>>> Matrix3d<T>::addition(const vector<vector<vector<T>>> matrixA,
+                                                const vector<vector<vector<T>>> matrixB)
+{
+
+}
+
+template<typename T>
+vector<vector<vector<T>>> Matrix3d<T>::addition(const vector<vector<vector<T>>> matrix,
+                                                T num)
+{
+
+}
+
+template<typename T>
+vector<vector<vector<T>>> Matrix3d<T>::subtraction(const vector<vector<vector<T>>> matrixA,
+                                                   const vector<vector<vector<T>>> matrixB)
+{
+
+}
+
+template<typename T>
+vector<vector<vector<T>>> Matrix3d<T>::subtraction(const vector<vector<vector<T>>> matrix,
+                                                   T num)
+{
+
+}
+
+template<typename T>
+vector<vector<vector<T>>> Matrix3d<T>::multiplication(const vector<vector<vector<T>>> matrixA,
+                                                      const vector<vector<vector<T>>> matrixB)
+{
+
+}
+
+template<typename T>
+vector<vector<vector<T>>> Matrix3d<T>::multiplication(const vector<vector<vector<T>>> matrix,
+                                                      T num)
+{
+
+}
+
+template<typename T>
+vector<vector<vector<T>>> Matrix3d<T>::transposition(const vector<vector<vector<T>>> matrix)
+{
+
+}
---
+++ src/matrix/matrix3d.h
@@ -0,0 +1,47 @@
+#ifndef MATRIX3D_H
+#define MATRIX3D_H
+
+#include <constants.h>
+#include <vector>
+
+using namespace std;
+
+///
+/// \brief The Matrix3d class
+/// определение основных типов операций
+/// над элементами трехмерного матричного типа
+template<typename T>
+class Matrix3d
+{
+public:
+    // соответствие матриц
+    static bool sameShape(const vector<vector<vector<T>>> matrixA,
+                          const vector<vector<vector<T>>> matrixB);
+
+    // сумма матриц (или матрицы и числа)
+    static vector<vector<vector<T>>>
+        addition(const vector<vector<vector<T>>> matrixA,
+                 const vector<vector<vector<T>>> matrixB);
+    static vector<vector<vector<T>>>
+        addition(const vector<vector<vector<T>>> matrix, T num);
+
+    // разность матриц (или матрицы и числа)
+    static vector<vector<vector<T>>>
+        subtraction(const vector<vector<vector<T>>> matrixA,
+                    const vector<vector<vector<T>>> matrixB);
+    static vector<vector<vector<T>>>
+        subtraction(const vector<vector<vector<T>>> matrix, T num);
+
+    // умножение матриц (или матрицы и числа)
+    static vector<vector<vector<T>>>
+        multiplication(const vector<vector<vector<T>>> matrixA,
+                       const vector<vector<vector<T>>> matrixB);
+    static vector<vector<vector<T>>>
+        multiplication(const vector<vector<vector<T>>> matrix, T num);
+
+    // транспозиция матрицы
+    static vector<vector<vector<T>>>
+        transposition(const vector<vector<vector<T>>> matrix);
+};
+
+#endif // MATRIX3D_H
