565fe3b2aadeb520cae95a7c2e0eaa2018061c9b
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-31 11:53:35 +0300

    Новый тест для перемножения матриц в виде векторов.

--- test_matrix/testmatrix.cpp
+++ test_matrix/testmatrix.cpp
@@ -13,6 +13,10 @@ private slots:
     /// \brief testMatrix2dMultNumber
     /// умножение матрицы на число
     void testMatrix2dMultNumber();
+    ///
+    /// \brief test1MatrixMultMatrix
+    /// первое тестовое умножение матриц
+    void test1MatrixMultMatrix();
 };
 
 void TestMatrix::testMatrix2dMultNumber()
@@ -25,6 +29,23 @@ void TestMatrix::testMatrix2dMultNumber()
     vector<vector<double>> properMatrix {{-2,-4,-6},
                                          {-14,-6,-10},
                                          {-18,-6,-2}};
+    QCOMPARE(resultMatrix, properMatrix);
+}
+
+void TestMatrix::test1MatrixMultMatrix()
+{
+    vector<vector<double>> matrixA {{1,2,3},
+                                    {3,4,5},
+                                    {6,8,2}};
+    vector<vector<double>> matrixB {{1,6,2},
+                                    {3,2,2},
+                                    {2,2,8}};
+    vector<vector<double>> resultMatrix
+        = Matrix2d<double>::multiplication(matrixA, matrixB);
+
+    vector<vector<double>> properMatrix {{13,16,30},
+                                         {25,36,54},
+                                         {34,56,44}};
     QCOMPARE(resultMatrix, properMatrix);
 }
 
