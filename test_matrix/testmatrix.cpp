f204780cd4cb74b11a4c2af1a876b2738722ccf0
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-05-31 09:54:38 +0300

    Тест умножения матрицы на число.
    + исправление мелких недочетов.

--- test_matrix/testmatrix.cpp
+++ test_matrix/testmatrix.cpp
@@ -1,16 +1,32 @@
 #include <QObject>
 #include <QtTest/QtTest>
 
-#include <matrix.h>
-
-using namespace std;
+#include <matrix2d.h>
+#include <matrix2d.cpp>
 
 class TestMatrix: public QObject
 {
     Q_OBJECT
 
 private slots:
+    ///
+    /// \brief testMatrix2dMultNumber
+    /// умножение матрицы на число
+    void testMatrix2dMultNumber();
 };
+
+void TestMatrix::testMatrix2dMultNumber()
+{
+    vector<vector<double>> matrix {{1,2,3},
+                                   {7,3,5},
+                                   {9,3,1}};
+    vector<vector<double>> resultMatrix
+        = Matrix2d<double>::multiplication(matrix, -2);
+    vector<vector<double>> properMatrix {{-2,-4,-6},
+                                         {-14,-6,-10},
+                                         {-18,-6,-2}};
+    QCOMPARE(resultMatrix, properMatrix);
+}
 
 QTEST_MAIN(TestMatrix)
 #include "testmatrix.moc"
