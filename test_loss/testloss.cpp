0b9ad1eb795a9e731d5c113c4a33b7538fecb4c8
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-01 18:21:37 +0300

    Повышение читаемости кода для тестов потерь.

--- test_loss/testloss.cpp
+++ test_loss/testloss.cpp
@@ -24,22 +24,25 @@ private slots:
 
 void TestLoss::testSoftmax()
 {
+    // инициализация
     vector<double> init{5,3,2};
-    vector<double> currentResult
-        = ActivationFunctions::softmax(init);
+    // итоговый и ожидаемый результаты
+    vector<double> currentResult = ActivationFunctions::softmax(init);
     for (double &value : currentResult) {
         value = round(value * 1000)/1000;
     }
-
     vector<double> properResult{0.844, 0.114, 0.042};
+
     QCOMPARE(currentResult, properResult);
 }
 
 void TestLoss::testBatchSoftmax()
 {
+    // инициализация
     vector<vector<double>> init{{5,3,2},
                                 {7,1,5},
                                 {3,1,4}};
+    // итоговый и ожидаемый результаты
     vector<vector<double>> currentResult
         = ActivationFunctions::batchSoftmax(init);
     for (vector<double> &p : currentResult) {
@@ -47,10 +50,10 @@ void TestLoss::testBatchSoftmax()
             value = round(value * 1000)/1000;
         }
     }
-
     vector<vector<double>> properResult{{0.844, 0.114, 0.042},
                                         {0.879, 0.002, 0.119},
                                         {0.259, 0.035, 0.705}};
+
     QCOMPARE(currentResult, properResult);
 }
 
