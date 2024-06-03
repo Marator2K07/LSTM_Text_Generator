cda0815d63bbcdff44f5cc3fea4e6e3fe88f9f82
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-03 10:42:26 +0300

    Написан тест для нахождения потери(штрафа) сети.

--- test_loss/testloss.cpp
+++ test_loss/testloss.cpp
@@ -3,6 +3,7 @@
 #include <QtTest/QtTest>
 
 #include "activationfunctions.h"
+#include "softmaxcrossentropyloss.h"
 
 using namespace std;
 
@@ -20,6 +21,11 @@ private slots:
     /// \brief testBatchSoftmax расширенный тест для
     /// обработки партии предсказаний
     void testBatchSoftmax();
+    ///
+    /// \brief testLossCalculate тестирование
+    /// вычисления штрафа сети в виде double числа
+    /// при прямом проходе
+    void testSoftmaxCrossEntropyLossForward();
 };
 
 void TestLoss::testSoftmax()
@@ -55,6 +61,36 @@ void TestLoss::testBatchSoftmax()
                                         {0.259, 0.035, 0.705}};
 
     QCOMPARE(currentResult, properResult);
+}
+
+void TestLoss::testSoftmaxCrossEntropyLossForward()
+{
+    // инициализация
+    SoftmaxCrossEntropyLoss SCELoss;
+    vector<vector<double>> target{{5,3,2},
+                                  {7,6,11},
+                                  {7,1,3}};
+    vector<vector<double>> predictionBad{{5,3},
+                                         {7,6},
+                                         {7,1}};
+    vector<vector<double>> prediction{{5.55,3.33,2.12},
+                                      {4.26,10.10,2.25},
+                                      {3.33,1.19,7.12}};
+    // результаты
+    double resultPenalty = SCELoss.forward(prediction, target);
+    resultPenalty = floor(resultPenalty * 1000) / 1000;
+    double properPenalty = 130.139;
+
+    QCOMPARE(resultPenalty, properPenalty);
+
+    try {
+        double resultPenalty = SCELoss.forward(predictionBad, target);
+
+        // Т.к. код до сюда не дойдет, тест будет пройден
+        QCOMPARE(resultPenalty, properPenalty);
+    } catch (const LossException &e) {
+        cerr << e.what() << endl;
+    }
 }
 
 QTEST_MAIN(TestLoss)
