37750c22a8f20fda403ea8da704903b5ae5b4327
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-05 08:11:18 +0300

    Дописан функционал интерфейса.

--- src/textembedding/itextembedding.h
+++ src/textembedding/itextembedding.h
@@ -1,15 +1,25 @@
 #ifndef ITEXTEMBEDDING_H
 #define ITEXTEMBEDDING_H
 
+#include <vector>
+
+using namespace std;
+
 ///
 /// \brief The ITextEmbedding interface
 /// определение функциональности преобразователя
 /// какой-то абстрактной информации(текста) в математическое
-/// представление (набор из векторов - или матрицы)
+/// представление (набор векторов - или матрицы)
 class ITextEmbedding
 {
 public:
-    ITextEmbedding();
+    virtual vector<vector<int>> genInputIndices(int startPos) = 0;
+    virtual vector<vector<int>> genTargetIndices(int startPos) = 0;
+    virtual vector<vector<vector<int>>>
+    genNewBanch(vector<vector<int>> indices) = 0;
+
+public:
+    virtual ~ITextEmbedding() {}
 };
 
 #endif // ITEXTEMBEDDING_H
