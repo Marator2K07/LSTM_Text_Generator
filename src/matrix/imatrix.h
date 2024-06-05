e979ee8d7306429c74ca0ae92cb8430970c6a347
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-05 18:58:50 +0300

    Новый метод для интерфейса матриц.

--- src/matrix/imatrix.h
+++ src/matrix/imatrix.h
@@ -18,6 +18,12 @@ template<typename T>
 class IMatrix
 {
 public:
+    ///
+    /// \brief sameShape проверка на полное
+    /// соотвествие размеров этой и другой матрицы
+    /// \param other другая матрица
+    /// \return флаг соответствия всех размеров
+    virtual bool sameShape(const IMatrix<T> *other) = 0;
 
 public:
     virtual ~IMatrix() {}
