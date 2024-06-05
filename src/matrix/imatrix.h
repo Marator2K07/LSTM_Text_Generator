ee5dfc23e47b1772ec588779c41504437a2ef842
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-05 08:18:07 +0300

    Все-таки было решено добавить интерфейс для матриц.

---
+++ src/matrix/imatrix.h
@@ -0,0 +1,19 @@
+#ifndef IMATRIX_H
+#define IMATRIX_H
+
+#include <vector>
+
+using namespace std;
+
+///
+/// \brief The IMatrix interface
+/// определение функциональности для
+/// вложенных векторов - матриц
+template<typename T>
+class IMatrix
+{
+public:
+
+};
+
+#endif // IMATRIX_H
