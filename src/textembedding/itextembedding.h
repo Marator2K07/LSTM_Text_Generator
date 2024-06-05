f090503a515f594f0f7d9b7ee684ff9b744b67be
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-05 07:36:54 +0300

    Создание новой библиотеки для эмбеддинга.
    добавлен основной интерфейс только с комментарием.

---
+++ src/textembedding/itextembedding.h
@@ -0,0 +1,15 @@
+#ifndef ITEXTEMBEDDING_H
+#define ITEXTEMBEDDING_H
+
+///
+/// \brief The ITextEmbedding interface
+/// определение функциональности преобразователя
+/// какой-то абстрактной информации(текста) в математическое
+/// представление (набор из векторов - или матрицы)
+class ITextEmbedding
+{
+public:
+    ITextEmbedding();
+};
+
+#endif // ITEXTEMBEDDING_H
