105e3e5c2933f2c9562719c80a8d8bfa1bf713cd
Marator2K07 <dmitriyshaaa@gmail.com>
2024-06-08 22:51:41 +0300

    Чистка лишнего + повышение читаемости кода.

--- src/loss/activationfunctions.cpp
+++ src/loss/activationfunctions.cpp
@@ -4,24 +4,13 @@ vector<double> ActivationFunctions::softmax(const vector<double> prediction)
 {
     // сначала находим общий знаменатель
     double denominator = 0;
-    for (const double value : p) {
+    for (const double value : prediction) {
         denominator += exp(value);
     }
     // следующим проходом составляем обработанный вектор
     vector<double> result;
-    for (const double value : p) {
+    for (const double value : prediction) {
         result.push_back(exp(value)/denominator);
-    }
-    return result;
-}
-
-unique_ptr<IMatrix<double>>
-ActivationFunctions::batchSoftmax(const IMatrix<double> *predictions)
-{
-    // построчно обрабатываем предсказания
-    vector<vector<double>> result;
-    for (const vector<double> &p : predictions) {
-        result.push_back(softmax(p));
     }
     return result;
 }
