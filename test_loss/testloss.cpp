8d4fea98f9aa75fe3dc9674d3bac0354b9ab990f
Marator2K07 <dmitriyshaaa@gmail.com>
2024-06-08 22:50:01 +0300

    Тесты переписаны под новые условия.

--- test_loss/testloss.cpp
+++ test_loss/testloss.cpp
@@ -1,9 +1,11 @@
 #include <QObject>
-#include <vector>
 #include <QtTest/QtTest>
 
-#include "activationfunctions.h"
 #include "softmaxcrossentropyloss.h"
+#include "matrix2d.h"
+#include "matrix2d.cpp"
+#include "matrix3d.h"
+#include "matrix3d.cpp"
 
 using namespace std;
 
@@ -17,10 +19,6 @@ private slots:
     /// функция, - более чувствительное отношение
     /// к максимальным значениям
     void testSoftmax();
-    ///
-    /// \brief testBatchSoftmax расширенный тест для
-    /// обработки партии предсказаний
-    void testBatchSoftmax();
     ///
     /// \brief testSoftmaxCrossEntropyLossFull тестирование
     /// вычисления штрафа сети и градиента входных данных
@@ -46,56 +44,36 @@ void TestLoss::testSoftmax()
     QCOMPARE(currentResult, properResult);
 }
 
-void TestLoss::testBatchSoftmax()
-{
-    // инициализация
-    vector<vector<double>> init{{5,3,2},
-                                {7,1,5},
-                                {3,1,4}};
-    // итоговый и ожидаемый результаты
-    vector<vector<double>> currentResult
-        = ActivationFunctions::batchSoftmax(init);
-    Matrix2d<double>::floorM(currentResult, 1000);
-    vector<vector<double>> properResult{{0.843, 0.114, 0.042},
-                                        {0.878, 0.002, 0.118},
-                                        {0.259, 0.035, 0.705}};
-
-    QCOMPARE(currentResult, properResult);
-}
-
 void TestLoss::testSoftmaxCrossEntropyLossFull()
 {
     // инициализация
     SoftmaxCrossEntropyLoss SCELoss;
-    vector<vector<double>> target{{0.55,0.33,0.22},
-                                  {0.78,0.67,0.11},
-                                  {0.77,0.11,0.33}};
-    vector<vector<double>> predictionBad{{5,3},
-                                         {7,6},
-                                         {7,1}};
-    vector<vector<double>> prediction{{5.55,3.33,2.12},
-                                      {4.26,10.10,2.25},
-                                      {3.33,1.19,7.12}};
+    Matrix2d<double> target{{0.55,0.33,0.22},
+                            {0.78,0.67,0.11},
+                            {0.77,0.11,0.33}};
+    Matrix2d<double> predictionBad{{5,3},
+                                   {7,6},
+                                   {7,1}};
+    Matrix2d<double> prediction{{5.55,3.33,2.12},
+                                {4.26,10.10,2.25},
+                                {3.33,1.19,7.12}};
     // результаты (штраф или потеря)
-    double resultPenalty = SCELoss.forward(prediction, target);
+    double resultPenalty = SCELoss.forward(&prediction, &target);
     resultPenalty = floor(resultPenalty * 1000) / 1000;
     double properPenalty = 16.063;
     // результаты (градиент)
-    vector<vector<double>> resultGradient = SCELoss.backward();
-    Matrix2d<double>::floorM(resultGradient, 1000);
-    vector<vector<double>> properGradient{{0.326,-0.235,-0.192},
-                                          {-0.778,0.326,-0.11},
-                                          {-0.748,-0.108,0.645}};
+    auto resultGradient = SCELoss.backward();
+    resultGradient->floorM(3);
+    Matrix2d<double> properGradient{{0.326,-0.235,-0.192},
+                                    {-0.778,0.326,-0.11},
+                                    {-0.748,-0.108,0.645}};
 
     QCOMPARE(resultPenalty, properPenalty);
-    QCOMPARE(resultGradient, properGradient);
+    QCOMPARE(resultGradient->data(), properGradient.data());
 
     // случай с исключением
     try {
-        double resultPenalty = SCELoss.forward(predictionBad, target);
-
-        // Т.к. код до сюда не дойдет, тест будет пройден
-        QCOMPARE(resultPenalty, properPenalty);
+        double resultPenalty = SCELoss.forward(&predictionBad, &target);
     } catch (const LossException &e) {
         cerr << e.what() << endl;
     }
@@ -105,22 +83,22 @@ void TestLoss::expertTestSoftmaxCrossEntropyLoss()
 {
     // инициализация
     SoftmaxCrossEntropyLoss SCELoss;
-    vector<vector<double>> target{{0.55,0.33,0.22},
-                                  {0.78,0.67,0.11},
-                                  {0.77,0.11,0.33}};
-    vector<vector<double>> predictionOne{{5.55,3.33,2.12},
-                                         {4.26,10.10,2.25},
-                                         {3.33,1.19,7.12}};
-    vector<vector<double>> predictionTwo{{5.45,3.14,2.12},
-                                         {7.16,5.79,10.99},
-                                         {6.98,1.09,3.14}};
-    vector<vector<double>> predictionThree{{15.55,33.33,9.12},
-                                           {14.26,10.10,9.25},
-                                           {3.33,15.19,17.12}};
+    Matrix2d<double> target{{0.55,0.33,0.22},
+                            {0.78,0.67,0.11},
+                            {0.77,0.11,0.33}};
+    Matrix2d<double> predictionOne{{5.55,3.33,2.12},
+                                  {4.26,10.10,2.25},
+                                  {3.33,1.19,7.12}};
+    Matrix2d<double> predictionTwo{{5.45,3.14,2.12},
+                                  {7.16,5.79,10.99},
+                                  {6.98,1.09,3.14}};
+    Matrix2d<double> predictionThree{{15.55,33.33,9.12},
+                                    {14.26,10.10,9.25},
+                                    {3.33,15.19,17.12}};
     // результаты (штрафы или потери)
-    double resultPenaltyOne = SCELoss.forward(predictionOne, target);
-    double resultPenaltyTwo = SCELoss.forward(predictionTwo, target);
-    double resultPenaltyThree = SCELoss.forward(predictionThree, target);
+    double resultPenaltyOne = SCELoss.forward(&predictionOne, &target);
+    double resultPenaltyTwo = SCELoss.forward(&predictionTwo, &target);
+    double resultPenaltyThree = SCELoss.forward(&predictionThree, &target);
 
     QCOMPARE(resultPenaltyTwo < resultPenaltyOne, true);
     QCOMPARE(resultPenaltyTwo < resultPenaltyThree, true);
