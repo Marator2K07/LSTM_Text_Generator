aa747e2582b5ac14367b5df83ae0cbb93aed74ce
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-09 11:53:59 +0300

    Обновленные сигнатуры интерфейса эмбеддера.
    + правки в комментарии интерфейса и подключение matrix/

--- src/textembedding/itextembedding.h
+++ src/textembedding/itextembedding.h
@@ -2,21 +2,22 @@
 #define ITEXTEMBEDDING_H
 
 #include <vector>
+#include "matrix2d.h"
+#include "matrix3d.h"
 
 using namespace std;
 
 ///
 /// \brief The ITextEmbedding interface
 /// определение функциональности преобразователя
-/// какой-то абстрактной информации(текста) в математическое
-/// представление (набор векторов - или матрицы)
+/// текста в математическое представление
+/// (набор вложенных векторов - или матрицы)
 class ITextEmbedding
 {
 public:
-    virtual vector<vector<int>> genInputIndices(int startPos) = 0;
-    virtual vector<vector<int>> genTargetIndices(int startPos) = 0;
-    virtual vector<vector<vector<int>>>
-    genNewBanch(vector<vector<int>> indices) = 0;
+    virtual Matrix2d<int> genInputIndices(int startPos) = 0;
+    virtual Matrix2d<int> genTargetIndices(int startPos) = 0;
+    virtual Matrix3d<int> genNewBanch(Matrix2d<int> indices) = 0;
 
 public:
     virtual ~ITextEmbedding() {}
