d0d716f965abb2df28ce82b9573ac63b78be1580
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-11 13:09:25 +0300

    Написан первый текст для текстового эмбеддинга.

--- test_textembedding/testtextembedding.cpp
+++ test_textembedding/testtextembedding.cpp
@@ -10,8 +10,31 @@ class TestTextEmbedding: public QObject
     Q_OBJECT
 
 private slots:
-
+    ///
+    /// \brief textFileProcessing
+    /// тест для начальной корректной обработки
+    /// данных при создании эксземпляров обькта
+    void textFileProcessing();
 };
+
+void TestTextEmbedding::textFileProcessing()
+{
+    try {
+        // инициализация
+        CharAsVectorEmbedding txtEmbed;
+        // результаты
+        QCOMPARE(txtEmbed.charToIdx().size(), txtEmbed.idxToChar().size());
+        QCOMPARE(txtEmbed.text().size() > txtEmbed.idxToChar().size(), true);
+        QCOMPARE(txtEmbed.text().size() > txtEmbed.charToIdx().size(), true);
+        QCOMPARE(txtEmbed.charToIdx()['1'] == txtEmbed.idxToChar()[49], true);
+        QCOMPARE(txtEmbed.charToIdx()['1'] == txtEmbed.idxToChar()[50], false);
+        for (int i = 0; i < txtEmbed.text().size(); ++i) {
+            cout << txtEmbed.charToIdx().value(txtEmbed.text()[i].toLatin1()) << " ";
+        }
+    } catch (const TextEmbeddingException &e) {
+        cout << e.what() << endl;
+    }
+}
 
 QTEST_MAIN(TestTextEmbedding)
 #include "testtextembedding.moc"
