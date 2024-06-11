7329096b86b9e569b61e1f14df68b55f951d120e
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-11 19:35:42 +0300

    Новый тест для текстового эмбеддинга.

--- test_textembedding/testtextembedding.cpp
+++ test_textembedding/testtextembedding.cpp
@@ -1,5 +1,6 @@
 #include <QObject>
 #include <QtTest/QtTest>
+#include <QMapIterator>
 
 #include "charasvectorembedding.h"
 
@@ -15,6 +16,10 @@ private slots:
     /// тест для начальной корректной обработки
     /// данных при создании эксземпляров обькта
     void textFileProcessing();
+    ///
+    /// \brief testGenTextEmbeddingIndices
+    /// тестирование генерации индексов символов в матрицу
+    void testGenTextEmbeddingIndices();
 };
 
 void TestTextEmbedding::textFileProcessing()
@@ -31,6 +36,30 @@ void TestTextEmbedding::textFileProcessing()
         for (int i = 0; i < txtEmbed.text().size(); ++i) {
             cout << txtEmbed.charToIdx().value(txtEmbed.text()[i].toLatin1()) << " ";
         }
+    } catch (const TextEmbeddingException &e) {
+        cout << e.what() << endl;
+    }
+}
+
+void TestTextEmbedding::testGenTextEmbeddingIndices()
+{
+    try {
+        // инициализация
+        CharAsVectorEmbedding txtEmbed("Plain_Kate.txt", 32, 16);
+        // результаты
+        Matrix2d<int> resIndices = txtEmbed.genTextIndices(0);
+        QString str = " Erin Bow";
+        for (int i = 0; i < str.size(); ++i) {
+            int idx = txtEmbed.charToIdx()[str[i].toLatin1()];
+            QCOMPARE(idx, (int)str[i].toLatin1());
+        }
+        // информация для анализа
+        QMapIterator<int, char> it(txtEmbed.idxToChar());
+        while (it.hasNext()) {
+            it.next();
+            cout << it.key() << ": " << it.value() << endl;
+        }
+        resIndices.print();
     } catch (const TextEmbeddingException &e) {
         cout << e.what() << endl;
     }
