94bdf61f8676de0aaeb7758cd4709c32b61e0ccd
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-12 19:19:11 +0300

    Не забываем включить папку в проект.
    + следующий вариант реализации для интерфейса.

--- src/optimizer/ioprimizer.h
+++ src/optimizer/ioprimizer.h
@@ -1,11 +1,21 @@
 #ifndef IOPRIMIZER_H
 #define IOPRIMIZER_H
 
-
+///
+/// \brief The IOprimizer interface
+/// определение функционала оптимизатора модели
+/// нейронной сети (обновление параметров сети при обучении)
 class IOprimizer
 {
+private:
+    ///
+    virtual void updateRule() = 0;
+
 public:
-    IOprimizer();
+    ///
+    /// \brief step шаг прохода по параметрам
+    /// модели сети для их обновления
+    virtual void step() = 0;
 };
 
 #endif // IOPRIMIZER_H
