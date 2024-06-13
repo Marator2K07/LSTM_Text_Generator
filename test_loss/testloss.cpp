8af0b8cd331ceb3a4e387eda40ae68e873708e4c
Marator2K07 <dmitriyshaaa@gmail.com>
2024-06-13 18:17:39 +0300

    Перенос метода тестирования softmax в нужное место.

--- test_loss/testloss.cpp
+++ test_loss/testloss.cpp
@@ -15,11 +15,6 @@ class TestLoss: public QObject
 
 private slots:
     ///
-    /// \brief testSoftmax многопеременная логистическая
-    /// функция, - более чувствительное отношение
-    /// к максимальным значениям
-    void testSoftmax();
-    ///
     /// \brief testSoftmaxCrossEntropyLossFull тестирование
     /// вычисления штрафа сети и градиента входных данных
     void testSoftmaxCrossEntropyLossFull();
@@ -29,20 +24,6 @@ private slots:
     /// зависимости от входных данных
     void expertTestSoftmaxCrossEntropyLoss();
 };
-
-void TestLoss::testSoftmax()
-{
-    // инициализация
-    vector<int> init{5,3,2};
-    // итоговый и ожидаемый результаты
-    vector<double> currentResult = ActivationFunctions<int>::softmax(init);
-    for (double &value : currentResult) {
-        value = round(value * 1000)/1000;
-    }
-    vector<double> properResult{0.844, 0.114, 0.042};
-
-    QCOMPARE(currentResult, properResult);
-}
 
 void TestLoss::testSoftmaxCrossEntropyLossFull()
 {
