854ebdf5dbc1999d1aaeb01e36e3066072784637
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-14 10:00:48 +0300

    Новый тест softmax для 2д матриц.

--- test_activationfunctions/testactivationfunctions.cpp
+++ test_activationfunctions/testactivationfunctions.cpp
@@ -20,7 +20,10 @@ private slots:
     /// функция, - более чувствительное отношение
     /// к максимальным значениям
     void testSoftmax();
-
+    ///
+    /// \brief testSoftmax2d
+    /// тестирование софтмакс в условиях двумерной матрицы
+    void testSoftmax2d();
 };
 
 void TestActivationFunctions::testSoftmax()
@@ -35,7 +38,22 @@ void TestActivationFunctions::testSoftmax()
     QCOMPARE(currentResult.data(), properResult.data());
 }
 
-    QCOMPARE(currentResult, properResult);
+void TestActivationFunctions::testSoftmax2d()
+{
+    Matrix2d<double> init{{5, 3, 2},
+                          {2, 3, 1},
+                          {6, 1, 8}};
+    // итоговый и ожидаемый результаты
+    Matrix2d<double> currentResult(
+        ActivationFunctions<double>::softmax(&init)->data()
+    );
+    currentResult.floorM(5);
+    Matrix2d<double> properResult{{0.84379, 0.11419, 0.04201},
+                                  {0.24472, 0.66524, 0.09003},
+                                  {0.1191, 0.0008, 0.88009}};
+
+    QCOMPARE(currentResult.data(), properResult.data());
+}
 }
 
 QTEST_MAIN(TestActivationFunctions)
