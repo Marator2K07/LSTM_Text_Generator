2ae70ca7b8311af5914bb75ce0913e2dcaddb99f
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-21 10:37:23 +0300

    Дописана реализация метода инициализации слоя.

--- src/neuralnetwork/lstmlayer.cpp
+++ src/neuralnetwork/lstmlayer.cpp
@@ -12,7 +12,80 @@ LSTMLayer::LSTMLayer(int hiddenSize, int outputSize, double weightScale)
 
 void LSTMLayer::initParams(const Matrix3d<double> initMatrix)
 {
-
+    _vocabSize = initMatrix.sizes()[2];
+    // преинициализация параметров слоя
+    _params.insert("W_f", QMap<QString, Matrix2d<double>>());
+    _params.insert("B_f", QMap<QString, Matrix2d<double>>());
+    _params.insert("W_i", QMap<QString, Matrix2d<double>>());
+    _params.insert("B_i", QMap<QString, Matrix2d<double>>());
+    _params.insert("W_c", QMap<QString, Matrix2d<double>>());
+    _params.insert("B_c", QMap<QString, Matrix2d<double>>());
+    _params.insert("W_o", QMap<QString, Matrix2d<double>>());
+    _params.insert("B_o", QMap<QString, Matrix2d<double>>());
+    _params.insert("W_v", QMap<QString, Matrix2d<double>>());
+    _params.insert("B_v", QMap<QString, Matrix2d<double>>());
+    // инициализация значений параметров слоя
+    _params["W_f"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, _weightScale,
+                                       _hiddenSize+_vocabSize, _hiddenSize)
+        );
+    _params["B_f"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, _weightScale, 1, _hiddenSize)
+        );
+    _params["W_i"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, _weightScale,
+                                       _hiddenSize+_vocabSize, _hiddenSize)
+        );
+    _params["B_i"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, _weightScale, 1, _hiddenSize)
+        );
+    _params["W_c"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, _weightScale,
+                                       _hiddenSize+_vocabSize, _hiddenSize)
+        );
+    _params["B_c"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, _weightScale, 1, _hiddenSize)
+        );
+    _params["W_o"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, _weightScale,
+                                       _hiddenSize+_vocabSize, _hiddenSize)
+        );
+    _params["B_o"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, _weightScale, 1, _hiddenSize)
+        );
+    _params["W_v"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, _weightScale,
+                                       _hiddenSize, _outputSize)
+        );
+    _params["B_v"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, _weightScale, 1, _outputSize)
+        );
+    // инициализация производных параметров слоя
+    const QList<QString> keys = _params.keys();
+    for (const QString &key : keys) {
+        _params[key].insert(
+            "deriv",
+            Matrix2d<double>::zeroM(
+                _params[key]["value"].sizes()[0],
+                _params[key]["value"].sizes()[1])
+            );
+    }
+    // инициализируем ячейки для нейронов/узлов
+    for (int i = 0; i < initMatrix.sizes()[1]; ++i) {
+        _cells.push_back(LSTMNode());
+    }
+    // сбрасываем флаг инициализации
+    _firstStep = false;
 }
 
 void LSTMLayer::clearGradients()
