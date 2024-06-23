7a496ffff6f309f06c989283d7e475254627996e
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-23 10:14:21 +0300

    Переработаны сигнатуры методов интерфейса.

--- src/neuralnetwork/ineuralnetworknode.h
+++ src/neuralnetwork/ineuralnetworknode.h
@@ -13,21 +13,28 @@
 class INeuralNetworkNode
 {
 public:
-    ///
     /// \brief forward прямой проход - вычисление прогнозируемых значений
-    /// \param input словарь входов для данных и скрытых состояний
+    /// \param xIn матрица входа данных
+    /// \param hIn матрица входа состояния слоя
+    /// \param cIn матрица входа состояния ячейки
     /// \param layerParams заданные параметры слоя для данного узла
     /// \return словарь выходных данных и скрытых состояний
     virtual QMap<QString, Matrix2d<double>>
-    forward(QMap<QString, Matrix2d<double>> input,
+    forward(Matrix2d<double> xIn,
+            Matrix2d<double> hIn,
+            Matrix2d<double> cIn,
             QMap<QString, QMap<QString, Matrix2d<double>>> &layerParams) = 0;
     ///
     /// \brief backward обратное распостранение градиента по узлу слоя
-    /// \param outputGrad словарь градиентов по выходу и скрытым состояниям
+    /// \param xOutGrad матрица градиентов по выходу для значений
+    /// \param hOutGrad матрица градиентов по выходу для состояния слоя
+    /// \param cOutGrad матрица градиентов по выходу для состояния ячейки
     /// \param layerParams заданные параметры слоя для данного узла
-    /// \return словарь градиентов по входным данным и скрытым состояниям
+    /// \return словарь градиентов по входным данным и входным скрытым состояниям
     virtual QMap<QString, Matrix2d<double>>
-    backward(QMap<QString, Matrix2d<double>> outputGrad,
+    backward(Matrix2d<double> xOutGrad,
+             Matrix2d<double> hOutGrad,
+             Matrix2d<double> cOutGrad,
              QMap<QString, QMap<QString, Matrix2d<double>>> &layerParams) = 0;
 };
 
