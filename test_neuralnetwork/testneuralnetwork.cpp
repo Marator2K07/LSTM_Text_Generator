811db265f1037a2c8840bdb4e0f0c89ed28dec78
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-24 20:45:27 +0300

    Добавлен 'тест под вопросом', прошлые слегка переработаны.

--- test_neuralnetwork/testneuralnetwork.cpp
+++ test_neuralnetwork/testneuralnetwork.cpp
@@ -13,6 +13,10 @@ class TestNeuralNetwork: public QObject
 
 private slots:
     ///
+    /// \brief testLSTMnode
+    /// простая проверка работоспособности узла типа LSTM вручную
+    void testLSTMnode();
+    ///
     /// \brief testLSTMLayerForward
     /// поверхностная проверка работоспособности прямого прохода слоя LSTM
     void testLSTMLayerForward();
@@ -22,18 +26,115 @@ private slots:
     void testLSTMLayerBackward();
 };
 
-void TestNeuralNetwork::testLSTMLayerForward()
+void TestNeuralNetwork::testLSTMnode()
 {
     // инициализация
-    int batchSize = 44;
-    int outputSize = 33;
-    int sequenceLenght = 8;
-    int vocabSize = 28;
-    LSTMLayer layer(batchSize, outputSize);
-    Matrix3d<double> sequenceIn
-        = Matrix3d<double>::zeroM(batchSize, sequenceLenght, vocabSize);
-    // расчеты
+    int hiddenSize = 12;
+    int batchSize = 6;
+    int outputSize = 4;
+    int vocabSize = 4;
+    LSTMNode node;
+    QMap<QString, QMap<QString, Matrix2d<double>>> layerParams;
+    layerParams.insert("W_f", QMap<QString, Matrix2d<double>>());
+    layerParams.insert("B_f", QMap<QString, Matrix2d<double>>());
+    layerParams.insert("W_i", QMap<QString, Matrix2d<double>>());
+    layerParams.insert("B_i", QMap<QString, Matrix2d<double>>());
+    layerParams.insert("W_c", QMap<QString, Matrix2d<double>>());
+    layerParams.insert("B_c", QMap<QString, Matrix2d<double>>());
+    layerParams.insert("W_o", QMap<QString, Matrix2d<double>>());
+    layerParams.insert("B_o", QMap<QString, Matrix2d<double>>());
+    layerParams.insert("W_v", QMap<QString, Matrix2d<double>>());
+    layerParams.insert("B_v", QMap<QString, Matrix2d<double>>());
+    layerParams["W_f"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, 0.01, hiddenSize+vocabSize, hiddenSize)
+        );
+    layerParams["B_f"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, 0.01, 1, hiddenSize)
+        );
+    layerParams["W_i"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, 0.01, hiddenSize+vocabSize, hiddenSize));
+    layerParams["B_i"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, 0.01, 1, hiddenSize)
+        );
+    layerParams["W_c"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, 0.01, hiddenSize+vocabSize, hiddenSize));
+    layerParams["B_c"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, 0.01, 1, hiddenSize)
+        );
+    layerParams["W_o"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, 0.01, hiddenSize+vocabSize, hiddenSize));
+    layerParams["B_o"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, 0.01, 1, hiddenSize)
+        );
+    layerParams["W_v"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, 0.01, hiddenSize, outputSize));
+    layerParams["B_v"].insert(
+        "value",
+        Matrix2d<double>::randomNormal(0.0, 0.01, 1, outputSize)
+        );
+
+    const QList<QString> keys = layerParams.keys();
+    for (const QString &key : keys) {
+        layerParams[key].insert(
+            "deriv",
+            Matrix2d<double>::zeroM(
+                layerParams[key]["value"].sizes()[0],
+                layerParams[key]["value"].sizes()[1])
+            );
+    }
+
     try {
+    QMap<QString, Matrix2d<double>> forwardMap
+        = node.forward(Matrix2d<double>::zeroM(batchSize, vocabSize),
+                       Matrix2d<double>::zeroM(batchSize, hiddenSize),
+                       Matrix2d<double>::zeroM(batchSize, hiddenSize),
+                       layerParams);
+    //forwardMap["X_out"].print();
+    //forwardMap["H_out"].print();
+    //forwardMap["C_out"].print();
+
+    vector<unsigned long long> properXoutSizes{batchSize, outputSize};
+    vector<unsigned long long> properHoutSizes{batchSize, hiddenSize};
+    vector<unsigned long long> properCoutSizes{batchSize, hiddenSize};
+    QCOMPARE(forwardMap["X_out"].sizes(), properXoutSizes);
+    QCOMPARE(forwardMap["H_out"].sizes(), properHoutSizes);
+    QCOMPARE(forwardMap["C_out"].sizes(), properCoutSizes);
+
+    //QMap<QString, Matrix2d<double>> backwardMap
+    //    = node.backward(Matrix2d<double>::zeroM(1, vocabSize),
+    //                    Matrix2d<double>::zeroM(1, hiddenSize),
+    //                    Matrix2d<double>::zeroM(1, hiddenSize),
+    //                    layerParams);
+    //forwardMap["dX_prev"].print();
+    //forwardMap["dH_prev"].print();
+    //forwardMap["dC_prev"].print();
+    } catch (const NeuralNetworkException &e) {
+        cout << e.what() << endl;
+    }
+}
+
+void TestNeuralNetwork::testLSTMLayerForward()
+{
+    try {
+        // инициализация
+        int hiddenSize = 128;
+        int outputSize = 44;
+        LSTMLayer layer(hiddenSize, outputSize);
+        int batchSize = 64;
+        int sequenceLenght = 10;
+        int vocabSize = 44;
+        Matrix3d<double> sequenceIn
+            = Matrix3d<double>::zeroM(batchSize, sequenceLenght, vocabSize);
+        // расчеты
         Matrix3d<double> sequenceOut = layer.forward(sequenceIn);
         //sequenceOut.print();
 
@@ -46,18 +147,19 @@ void TestNeuralNetwork::testLSTMLayerForward()
 void TestNeuralNetwork::testLSTMLayerBackward()
 {
     // инициализация
-    int batchSize = 64;
-    int outputSize = 44;
-    int sequenceLenght = 10;
-    int vocabSize = 44;
-    LSTMLayer layer(batchSize, outputSize);
-    Matrix3d<double> sequenceIn
-        = Matrix3d<double>::zeroM(batchSize, sequenceLenght, vocabSize);
-    Matrix3d<double> gradOut
-        = Matrix3d<double>::randomNormal(0.0, 0.01,
-                                         batchSize, sequenceLenght, vocabSize);
-    // расчеты
     try {
+        int hiddenSize = 128;
+        int outputSize = 44;
+        LSTMLayer layer(hiddenSize, outputSize);
+        int batchSize = 64;
+        int sequenceLenght = 10;
+        int vocabSize = 44;
+        Matrix3d<double> sequenceIn
+            = Matrix3d<double>::zeroM(batchSize, sequenceLenght, vocabSize);
+        Matrix3d<double> gradOut
+            = Matrix3d<double>::randomNormal(0.0, 0.01,
+                                             batchSize, sequenceLenght, vocabSize);
+        // расчеты
         Matrix3d<double> sequenceOut = layer.forward(sequenceIn);
         Matrix3d<double> gradIn = layer.backward(gradOut);
         gradIn.print();
