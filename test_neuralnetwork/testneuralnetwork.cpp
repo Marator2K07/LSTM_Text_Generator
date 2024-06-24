52a1e4b9a9412fb77d64d8913b30e9569b642c2b
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-24 17:26:09 +0300

    Добавлен новый тест и подправлен прошлый.

--- test_neuralnetwork/testneuralnetwork.cpp
+++ test_neuralnetwork/testneuralnetwork.cpp
@@ -16,6 +16,10 @@ private slots:
     /// \brief testLSTMLayerForward
     /// поверхностная проверка работоспособности прямого прохода слоя LSTM
     void testLSTMLayerForward();
+    ///
+    /// \brief testLSTMLayerBackward
+    /// поверхностная проверка работоспособности обратного прохода слоя LSTM
+    void testLSTMLayerBackward();
 };
 
 void TestNeuralNetwork::testLSTMLayerForward()
@@ -29,10 +33,39 @@ void TestNeuralNetwork::testLSTMLayerForward()
     Matrix3d<double> sequenceIn
         = Matrix3d<double>::zeroM(batchSize, sequenceLenght, vocabSize);
     // расчеты
-    Matrix3d<double> sequenceOut = layer.forward(sequenceIn);
-    sequenceOut.print();
+    try {
+        Matrix3d<double> sequenceOut = layer.forward(sequenceIn);
+        //sequenceOut.print();
 
-    QCOMPARE(sequenceIn == sequenceOut, false);
+        QCOMPARE(sequenceIn == sequenceOut, false);
+    } catch (const NeuralNetworkException &e) {
+        cout << e.what() << endl;
+    }
+}
+
+void TestNeuralNetwork::testLSTMLayerBackward()
+{
+    // инициализация
+    int batchSize = 64;
+    int outputSize = 44;
+    int sequenceLenght = 10;
+    int vocabSize = 44;
+    LSTMLayer layer(batchSize, outputSize);
+    Matrix3d<double> sequenceIn
+        = Matrix3d<double>::zeroM(batchSize, sequenceLenght, vocabSize);
+    Matrix3d<double> gradOut
+        = Matrix3d<double>::randomNormal(0.0, 0.01,
+                                         batchSize, sequenceLenght, vocabSize);
+    // расчеты
+    try {
+        Matrix3d<double> sequenceOut = layer.forward(sequenceIn);
+        Matrix3d<double> gradIn = layer.backward(gradOut);
+        gradIn.print();
+
+        QCOMPARE(gradOut == gradIn, false);
+    } catch (const NeuralNetworkException &e) {
+        cout << e.what() << endl;
+    }
 }
 
 QTEST_MAIN(TestNeuralNetwork)
