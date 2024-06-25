ff450a61cd009e703e8f2216320e53cb5daf9c66
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-25 20:56:21 +0300

    Дописана реализация нового основного метода.

--- src/optimizer/sgd.cpp
+++ src/optimizer/sgd.cpp
@@ -11,19 +11,34 @@ SGD::SGD(INeuralNetworkModel *model,
 
 void SGD::update()
 {
-    // получаем обновленное значение градиента
-    Matrix2d<double> updateGrad(
-        stepParams.value("grad").multiplication(_learningRate)->data()
-        );
-    // обновляем параметр сети
-    stepParams.insert(
-        "param",
-        Matrix2d<double>(
-            stepParams["param"].subtraction(&updateGrad)->data()
-            )
-        );
-}
-
-{
-
+    for (INeuralNetworkLayer *layer : _model->layers()) {
+        const QList<QString> layerKeys = layer->params().keys();
+        for (const QString &key : layerKeys) {
+            // ради удобства определяем первичный параметр слоя
+            QMap<QString, Matrix2d<double>> currentParam
+                = layer->params().value(key);
+            // если включена обрезка градиентов
+            if (_gradientClip) {
+                layer->updateParam(
+                    key,
+                    "deriv",
+                    Matrix2d<double>(
+                        currentParam["deriv"].clipM(-2, 2)->data()
+                        )
+                    );
+            }
+            // получаем обновленное значение градиента
+            Matrix2d<double> updateGrad(
+                currentParam["deriv"].multiplication(_learningRate)->data()
+                );
+            // обновляем параметр сети
+            layer->updateParam(
+                key,
+                "value",
+                Matrix2d<double>(
+                    currentParam["deriv"].subtraction(&updateGrad)->data()
+                    )
+                );
+        }
+    }
 }
