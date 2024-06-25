6cf8902f9f7a5f3e32284a588124e1063c150fbc
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-25 18:42:32 +0300

    Добавлена реализация для метода updateRule.

--- src/optimizer/sgd.cpp
+++ src/optimizer/sgd.cpp
@@ -8,7 +8,17 @@ SGD::SGD(double learningRate, bool gradientClip)
 
 void SGD::updateRule(QMap<QString, Matrix2d<double>> &stepParams)
 {
-
+    // получаем обновленное значение градиента
+    Matrix2d<double> updateGrad(
+        stepParams.value("grad").multiplication(_learningRate)->data()
+        );
+    // обновляем параметр сети
+    stepParams.insert(
+        "param",
+        Matrix2d<double>(
+            stepParams["param"].subtraction(&updateGrad)->data()
+            )
+        );
 }
 
 void SGD::step()
