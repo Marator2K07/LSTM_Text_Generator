b4ae8945c82834efb292de73f9abc59d59728f5e
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-25 17:21:38 +0300

    Дописали интерфейс для оптимизатора.
    + добавили необходимые подключения в библиотеку.

--- src/optimizer/ioprimizer.h
+++ src/optimizer/ioprimizer.h
@@ -1,20 +1,23 @@
 #ifndef IOPRIMIZER_H
 #define IOPRIMIZER_H
 
+#include <QMap>
+#include "matrix2d.h"
+
 ///
 /// \brief The IOprimizer interface
 /// определение функционала оптимизатора модели
-/// нейронной сети (обновление параметров сети при обучении)
+/// нейронной сети(обновление параметров сети при обучении)
 class IOprimizer
 {
-private:
-    ///
-    virtual void updateRule() = 0;
-
 public:
     ///
-    /// \brief step шаг прохода по параметрам
-    /// модели сети для их обновления
+    /// \brief updateRule обновление модели на основе переданных параметров
+    /// \param stepParams переданные параметры
+    virtual void updateRule(QMap<QString, Matrix2d<double>&> stepParams) = 0;
+    ///
+    /// \brief step обновление параметров модели путем
+    /// применения правила обновления и дополнительных возможностей
     virtual void step() = 0;
 };
 
