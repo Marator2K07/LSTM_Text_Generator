b4a31f5317d4c4694e9a7b1538bf0851c269955a
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-25 10:13:17 +0300

    Дописан функционал интерфейса модели нейронной сети.

--- src/neuralnetwork/ineuralnetworkmodel.h
+++ src/neuralnetwork/ineuralnetworkmodel.h
@@ -1,11 +1,33 @@
 #ifndef INEURALNETWORKMODEL_H
 #define INEURALNETWORKMODEL_H
 
+#include "lstmlayer.h"
 
+///
+/// \brief The INeuralNetworkModel interface
+/// организация функциональности модели нейронной сети
 class INeuralNetworkModel
 {
 public:
-    INeuralNetworkModel();
+    ///
+    /// \brief forward прямой проход по слоям модели
+    /// \param batch трехмерное представление входящих символов
+    /// \return трехмерное предсказание после обработки слоями
+    virtual Matrix3d<double> forward(Matrix3d<double> batch) = 0;
+    ///
+    /// \brief backward градиентный спуск по слоям сети
+    /// \param gradient конечный градиент потерь
+    /// \return итоговый(стартовый) градиент после прохода по слоям
+    virtual Matrix3d<double> backward(Matrix3d<double> gradient) = 0;
+    ///
+    /// \brief singleStep один проход по нейронной модели:
+    /// нахождение предсказания модели, вычисление потерь,
+    /// обратный проход и обновление параметров сети.
+    /// \param xBatch входная матрица
+    /// \param yBatch целевая матриц
+    /// \return потери при проходе сети
+    virtual double singleStep(Matrix3d<double> xBatch,
+                              Matrix3d<double> yBatch) = 0;
 };
 
 #endif // INEURALNETWORKMODEL_H
