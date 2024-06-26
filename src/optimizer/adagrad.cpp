23a7594559298d2ba56ccc83f6a3bfe7dc2a1cec
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-26 12:56:41 +0300

    Этап стартовой инициализации поля оптимизатора.

--- src/optimizer/adagrad.cpp
+++ src/optimizer/adagrad.cpp
@@ -11,5 +11,28 @@ AdaGrad::AdaGrad(INeuralNetworkModel *model,
 
 void AdaGrad::update()
 {
-
+    // при первом проходе инициализируем словарь хранения суммы
+    // квадратов градиентов для каждого параметра каждого слоя
+    if (_first) {
+        int layerIndex = 0;
+        for (INeuralNetworkLayer *layer : _model->layers()) {
+            _sumSquares.push_back(QMap<QString, Matrix2d<double>>());
+            const QList<QString> layerKeys = layer->params().keys();
+            for (const QString &key : layerKeys) {
+                // ради удобства определяем первичный параметр слоя
+                QMap<QString, Matrix2d<double>> currentParam
+                    = layer->params().value(key);
+                // сама инициализация
+                _sumSquares[layerIndex].insert(
+                    key,
+                    Matrix2d<double>::zeroM(
+                        currentParam["value"].sizes()[0],
+                        currentParam["value"].sizes()[1]
+                        )
+                    );
+            }
+            layerIndex++;
+        }
+        _first = false;
+    }
 }
