ebc6f1d71abc564aa28943dbe46333ef27dfc40a
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-26 20:59:48 +0300

    Дописана реализация главного метода класса AdaGrad.

--- src/optimizer/adagrad.cpp
+++ src/optimizer/adagrad.cpp
@@ -37,4 +37,55 @@ void AdaGrad::update()
         }
         _first = false;
     }
+    // каждого параметра каждого слоя
+    int layerIndex = 0;
+    for (INeuralNetworkLayer *layer : _model->layers()) {
+        const QList<QString> layerKeys = layer->params().keys();
+        for (const QString &key : layerKeys) {
+            // ради удобства определяем первичный параметр слоя
+            QMap<QString, Matrix2d<double>> currentParam
+                = layer->params().value(key);
+            // если включена обрезка градиентов
+            if (_gradientClip) {
+                layer->updateParam(
+                    key,
+                    "deriv",
+                    Matrix2d<double>(
+                        currentParam["deriv"].clipM(-2, 2)->data()
+                        )
+                    );
+            }
+            // вычисление новой суммы квадратов градиента
+            Matrix2d<double> newSumSquare(
+                currentParam["deriv"]
+                    .simplifiedMult(&currentParam["deriv"])
+                    ->addition(_epsilon)
+                    ->data()
+                );
+            _sumSquares[layerIndex].insert(
+                key,
+                Matrix2d<double>(
+                    _sumSquares[layerIndex].value(key)
+                        .addition(&newSumSquare)->data()
+                    )
+                );
+            // масштабирование скорости обучения
+            Matrix2d<double> learningRateMatrix(
+                _sumSquares[layerIndex].value(key)
+                    .sqrtM()
+                    ->dividing(_learningRate, true)
+                    ->data()
+                );
+            // обновление параметра сети
+            Matrix2d<double> newValue(
+                currentParam["value"]
+                    .subtraction(
+                        learningRateMatrix.simplifiedMult(&currentParam["deriv"]).get()
+                        )
+                    ->data()
+                );
+            layer->updateParam(key, "value", newValue);
+        }
+        layerIndex++;
+    }
 }
