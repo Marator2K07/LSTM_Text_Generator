efcaf3fcb0c67bbeffb7220576ea3d9e706002d1
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-27 09:48:37 +0300

    Расширили и поменяли функционал интерфейса тренера сети.

--- src/trainer/itrainer.h
+++ src/trainer/itrainer.h
@@ -8,11 +8,20 @@ class ITrainer
 {
 public:
     ///
+    /// \brief sampleOutput вывод текста на основе
+    /// текущей модели и ее степени обученности
+    /// \param startCharIdx индекс символа для старта генерации
+    /// \param sampleLenght длина генерации
+    virtual void sampleOutput(int startCharIdx, int sampleLenght) = 0;
+    ///
     /// \brief train тренирует модель генерации текста
     /// \param iterCount количество шагов обучения
     /// \param textSample флаг наличия вывода сгенерированного
     /// текста, используя последнюю версию модели
-    void train(int iterCount, bool textSample = false);
+    /// \param sampleEvery частота вывода текста
+    virtual void train(int iterCount,
+                       bool textSample = false,
+                       int sampleEvery = 100) = 0;
 };
 
 #endif // ITRAINER_H
