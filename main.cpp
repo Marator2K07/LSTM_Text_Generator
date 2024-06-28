6d390728a109c637c95bdce6a63dfd879f306cec
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-28 10:48:35 +0300

    Тестовый код в основном методе.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
 #include <QCoreApplication>
-#include <iostream>
+
+#include "sgd.h"
+#include "lstmlayer.h"
+#include "lstmmodel.h"
+#include "softmaxcrossentropyloss.h"
+#include "consistenttrainer.h"
+#include "charasvectorembedding.h"
+#include "charasvectorembedding.cpp"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    // инициализация
+    int hiddenSize = 77;
+    int batchSize = 16;
+    int sequenceLenght = 12;
+    CharAsVectorEmbedding<double> embedding("Plain_Kate.txt", sequenceLenght, batchSize);
+    LSTMModel model(new SoftmaxCrossEntropyLoss(),
+                    QList<INeuralNetworkLayer *>{
+                        new LSTMLayer(hiddenSize, embedding.vocabSize())
+                    });
+    SGD optimizer(&model, 0.007, true);
+    ConsistentTrainer trainer(&model, &embedding, &optimizer,
+                              sequenceLenght, batchSize);
+    // расчеты
+    try {
+        trainer.train(500, true, 25);
+    } catch (const MatrixException &e) {
+        cout << e.what();
+    } catch (const NeuralNetworkException &e) {
+        cout << e.what();
+    } catch (const exception &e) {
+        cout << e.what();
+    }
 
     return a.exec();
 }
