b0d4ae360952b32ca4330611ec5847f5f50c0296
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-06-28 09:48:18 +0300

    Добавлен новый тест для AdaGrad.
    + подправлены значения переменных для предыдущего теста.

--- test_optimizer/testoptimizer.cpp
+++ test_optimizer/testoptimizer.cpp
@@ -2,6 +2,7 @@
 #include <QtTest/QtTest>
 
 #include "sgd.h"
+#include "adagrad.h"
 #include "lstmmodel.h"
 #include "lstmlayer.h"
 #include "softmaxcrossentropyloss.h"
@@ -20,20 +21,25 @@ private slots:
     /// тестирование работоспособности SGD
     /// оптимизатора для нейронной сети
     void testSGDOptimizer();
+    ///
+    /// \brief testAdaGradOptimizer
+    /// тестирование работоспособности AdaGrad
+    /// оптимизатора для нейронной сети
+    void testAdaGradOptimizer();
 };
 
 void TestOptimizer::testSGDOptimizer()
 {
     // инициализация
-    int hiddenSize = 128;
-    int batchSize = 32;
-    int sequenceLenght = 10;
+    int hiddenSize = 111;
+    int batchSize = 12;
+    int sequenceLenght = 22;
     CharAsVectorEmbedding<double> txtEmbed("Plain_Kate.txt", sequenceLenght, batchSize);
     LSTMModel lstmModel(new SoftmaxCrossEntropyLoss(),
                         QList<INeuralNetworkLayer *>{
                             new LSTMLayer(hiddenSize, txtEmbed.vocabSize())
                         });
-    SGD SGDOptimizer(&lstmModel, 0.001, true);
+    SGD SGDOptimizer(&lstmModel, 0.008, true);
     // расчеты
     try {
         Matrix2d<double> indicesPred = txtEmbed.genTextIndices(0);
@@ -44,12 +50,12 @@ void TestOptimizer::testSGDOptimizer()
         for (int i = 0; i < 3; ++i) {
             double resLossAfterFirstStep
                 = lstmModel.singleStep(batchPred, batchTarget);
-            cout << resLossAfterFirstStep << endl;
+            cout << i << "): " << resLossAfterFirstStep << endl;
             SGDOptimizer.update();
 
             double resLossAfterSecondStep
                 = lstmModel.singleStep(batchPred, batchTarget);
-            cout << resLossAfterSecondStep << endl;
+            cout << i << "): " << resLossAfterSecondStep << endl;
             SGDOptimizer.update();
 
             QCOMPARE(resLossAfterFirstStep > resLossAfterSecondStep, true);
@@ -61,8 +67,47 @@ void TestOptimizer::testSGDOptimizer()
     } catch (const exception &e) {
         cout << e.what();
     }
+}
 
+void TestOptimizer::testAdaGradOptimizer()
+{
+    // инициализация
+    int hiddenSize = 128;
+    int batchSize = 16;
+    int sequenceLenght = 12;
+    CharAsVectorEmbedding<double> txtEmbed("Plain_Kate.txt", sequenceLenght, batchSize);
+    LSTMModel lstmModel(new SoftmaxCrossEntropyLoss(),
+                        QList<INeuralNetworkLayer *>{
+                            new LSTMLayer(hiddenSize, txtEmbed.vocabSize(), 0.01)
+                        });
+    AdaGrad AdaGradOptimizer(&lstmModel, 0.005);
+    // расчеты
+    try {
+        Matrix2d<double> indicesPred = txtEmbed.genTextIndices(0);
+        Matrix3d<double> batchPred = txtEmbed.genTextBatch(indicesPred);
+        Matrix2d<double> indicesTarget = txtEmbed.genTextIndices(1);
+        Matrix3d<double> batchTarget = txtEmbed.genTextBatch(indicesTarget);
 
+        for (int i = 0; i < 3; ++i) {
+            double resLossAfterFirstStep
+                = lstmModel.singleStep(batchPred, batchTarget);
+            cout << i << "): " << resLossAfterFirstStep << endl;
+            AdaGradOptimizer.update();
+
+            double resLossAfterSecondStep
+                = lstmModel.singleStep(batchPred, batchTarget);
+            cout << i << "): " << resLossAfterSecondStep << endl;
+            AdaGradOptimizer.update();
+
+            QCOMPARE(resLossAfterFirstStep > resLossAfterSecondStep, true);
+        }
+    } catch (const MatrixException &e) {
+        cout << e.what();
+    } catch (const NeuralNetworkException &e) {
+        cout << e.what();
+    } catch (const exception &e) {
+        cout << e.what();
+    }
 }
 
 QTEST_MAIN(TestOptimizer)
