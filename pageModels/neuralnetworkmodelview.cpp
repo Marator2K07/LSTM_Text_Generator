626e53418618da0651821b22683c40e2fcced216
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-07-03 19:19:23 +0300

    Создаем свой класс для представления списка моделей сети.
    начальная реализация.

---
+++ pageModels/neuralnetworkmodelview.cpp
@@ -0,0 +1,17 @@
+#include "neuralnetworkmodelview.h"
+
+NeuralNetworkModelView::NeuralNetworkModelView(QObject *parent)
+    : QAbstractListModel{parent}
+{
+
+}
+
+int NeuralNetworkModelView::rowCount(const QModelIndex &parent) const
+{
+
+}
+
+QVariant NeuralNetworkModelView::data(const QModelIndex &index, int role) const
+{
+
+}
