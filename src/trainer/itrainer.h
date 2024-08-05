5321a6301bb2f17fbfcdd019a35d13d53a2986ca
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-08-05 14:20:31 +0300

    Добавление в главный метод интерфейса заключительного параметра.

--- src/trainer/itrainer.h
+++ src/trainer/itrainer.h
@@ -37,9 +37,12 @@ public:
     /// \param textSample флаг наличия вывода сгенерированного
     /// текста, используя последнюю версию модели
     /// \param sampleEvery частота вывода текста
+    /// \param savePath путь сохранения данных модели
+    /// и тренера после завершения обучения(или во время обучения)
     virtual void train(int iterCount,
                        bool textSample = false,
-                       int sampleEvery = 100) = 0;
+                       int sampleEvery = 100,
+                       QString savePath = QDir::currentPath()) = 0;
     ///
     /// \brief updateStatus метод, для объединения
     /// посыла всех необходимых сигналов, связанных с соотвествующим
