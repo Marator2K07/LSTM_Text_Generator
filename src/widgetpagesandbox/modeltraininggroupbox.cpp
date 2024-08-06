4e8cf50f8404f5f5c11ace4524f8c2f2ed2e9e41
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-08-06 19:26:07 +0300

    Новый важный сигнал и его использование.

--- src/widgetpagesandbox/modeltraininggroupbox.cpp
+++ src/widgetpagesandbox/modeltraininggroupbox.cpp
@@ -201,13 +201,13 @@ void ModelTrainingGroupBox::loadExistingTrainer()
 
 void ModelTrainingGroupBox::trainModel()
 {
-    // в случае корректности данных, связанных с обучением
+    // в случае корректности данных, связанных с обучением,
+    // обновляем параметры для нового задания(обучения)
     if (trainPreDataIsCorrect()) {
-        _trainer->train(ui->iterTrainCountSpinBox->value(),
-                        ui->sampleOutputCheckBox->isChecked(),
-                        ui->sampleEverySpinBox->value(),
-                        ui->currentModelLineEdit->text()
-                        );
+        emit newAssignmentForTrain(ui->iterTrainCountSpinBox->value(),
+                                   ui->sampleOutputCheckBox->isChecked(),
+                                   ui->sampleEverySpinBox->value(),
+                                   ui->currentModelLineEdit->text());
     }
 }
 
