ceab4a9ec479ee4feb8445a3341750a0c0ad3b25
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-08-14 10:01:02 +0300

    Интерфейсу эмбеддинга теперь не нужен дженерик + QMap заменен на QHash.

--- src/textembedding/itextembedding.h
+++ src/textembedding/itextembedding.h
@@ -1,11 +1,10 @@
 #ifndef ITEXTEMBEDDING_H
 #define ITEXTEMBEDDING_H
 
-#include <QDir>
-#include <QMap>
-#include <QFile>
 #include <QByteArray>
-#include <vector>
+#include <QFile>
+#include <QHash>
+#include <QDir>
 
 #include "textembeddingexception.h"
 #include "matrix2d.h"
@@ -18,7 +17,6 @@ using namespace std;
 /// определение функциональности преобразователя
 /// текста в математическое представление
 /// (набор вложенных векторов - или матрицы)
-template<typename T>
 class ITextEmbedding
 {
 public:
@@ -45,11 +43,11 @@ public:
     ///
     /// \brief idxToChar геттер словаря индексов и букв для них
     /// \return словарь индекс->буква
-    virtual QMap<int, char> idxToChar() const = 0;
+    virtual QHash<int, char> idxToChar() const = 0;
     ///
     /// \brief charToIdx геттер словаря букв и индексов для них
     /// \return словарь буква->индекс
-    virtual QMap<char, int> charToIdx() const = 0;
+    virtual QHash<char, int> charToIdx() const = 0;
     ///
     /// \brief textToIndeces замена текста вектором индексов словаря
     /// \param text текст для анализа
@@ -60,13 +58,13 @@ public:
     /// индексов символов для заданного текста
     /// \param startPos точка старта генерации
     /// \return 2д матрица с индексами символов текста
-    virtual Matrix2d<T> genTextIndices(int startPos) = 0;
+    virtual Matrix2d<int> genTextIndices(int startPos) = 0;
     ///
     /// \brief genTextBatch генерация партии (3д матрицы)
     /// представлений символов для заданного текста
     /// \param indices 2д матрица индексов символов текста
     /// \return 3д матрица с матем. представлениями символов текста
-    virtual Matrix3d<T> genTextBatch(Matrix2d<T> indices) = 0;
+    virtual Matrix3d<int> genTextBatch(Matrix2d<int> indices) = 0;
 
 public:
     virtual ~ITextEmbedding() {}
