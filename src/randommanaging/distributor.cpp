c9d9ff6ee3c20d4e56ff1dc8680dea601936cf4b
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-08-18 16:47:54 +0300

    Добавлена новая библиотека для управления случайностями.
    + сразу создан будущий класс - синглотон (одиночка) без реализации.

---
+++ src/randommanaging/distributor.cpp
@@ -0,0 +1,6 @@
+#include "distributor.h"
+
+Distributor::Distributor()
+{
+
+}
---
+++ src/randommanaging/distributor.h
@@ -0,0 +1,14 @@
+#ifndef DISTRIBUTOR_H
+#define DISTRIBUTOR_H
+
+///
+/// \brief The Distributor class
+/// стандартный распределитель случайностей
+/// / данный класс является синглтоном /
+class Distributor
+{
+public:
+    Distributor();
+};
+
+#endif // DISTRIBUTOR_H
