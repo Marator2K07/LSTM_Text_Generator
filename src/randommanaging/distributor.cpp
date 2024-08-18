5888aa43faec28b8f13c7a81c8e17b8f1616abcf
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-08-18 19:37:25 +0300

    Предполагаемая стартовая реализация для распределителя.

--- src/randommanaging/distributor.cpp
+++ src/randommanaging/distributor.cpp
@@ -1,6 +1,14 @@
 #include "distributor.h"
 
-Distributor::Distributor()
-{
+Distributor *Distributor::distributor = nullptr;
 
+Distributor *Distributor::instance()
+{
+    // если поле не проинициализировано,
+    // то инициализируем его впервый и последний раз
+    if (distributor == nullptr) {
+        distributor = new Distributor;
+    }
+
+    return distributor;
 }
--- src/randommanaging/distributor.h
+++ src/randommanaging/distributor.h
@@ -7,8 +7,21 @@
 /// / данный класс является синглтоном /
 class Distributor
 {
-public:
+private:
+    // основное поле синглтона
+    static Distributor *distributor;
+
+private:
     Distributor();
+    ~Distributor();
+
+public:
+    ///
+    /// \brief instance получение текущего единственного
+    /// экземпляра распределителя случайностей
+    /// \return текущий единственный экземпляр распределителя
+    static Distributor *instance();
+
 };
 
 #endif // DISTRIBUTOR_H
